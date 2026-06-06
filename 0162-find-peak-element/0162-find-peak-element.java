2583f0a5dc3ca80766489e7dfa1609d29f6002e5
Harini0411 <bharathiharini2004@gmail.com>
2026-06-06 12:28:16 +0530

    Time: 0 ms (100%), Space: 44.3 MB (17.86%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,12 +3,11 @@ class Solution {
         int l=0;
         int r=nums.length-1;
         while(l<r){
-            int m = l+(r-l)/2;
+            int m =l+(r-l)/2;
             if(nums[m]<nums[m+1]){
                 l=m+1;
             }
-            else
-            {
+            else{
                 r=m;
             }
         }
