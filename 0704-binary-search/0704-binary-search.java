2c17ffdc9b375054d9463996745f2e1c141bb704
Harini0411 <bharathiharini2004@gmail.com>
2026-06-06 11:09:32 +0530

    Time: 0 ms (100%), Space: 48.1 MB (82.98%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -7,12 +7,13 @@ class Solution {
             if(nums[m]==target){
                 return m;
             }
-            else if(nums[m]<target){
+            else if (nums[m]<target){
                 l=m+1;
             }
             else{
                 r=m-1;
             }
+            
         }
         return -1;
     }
